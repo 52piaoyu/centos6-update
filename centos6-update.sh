3885700c870692a267f13a932244d7b00d9845d6
52piaoyu <52piaoyu@gmail.com>
2021-11-16 08:04:53 +0800

    Create centos6-update.sh

---
+++ centos6-update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sed -i "s|enabled=1|enabled=0|g" /etc/yum/pluginconf.d/fastestmirror.conf
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+curl -o /etc/yum.repos.d/CentOS-Base.repo https://www.xmpan.com/Centos-6-Vault-Aliyun.repo 
+yum clean all
+yum makecache
